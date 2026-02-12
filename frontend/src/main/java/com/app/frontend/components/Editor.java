7209de315ce286dd12f2a6c195b90ab01ced031e
Alaa-isa22 <ahamadmmmmm12345@gmail.com>
2026-02-12 12:56:54 +0300

    Editor update

--- frontend/src/main/java/com/app/frontend/components/Editor.java
+++ frontend/src/main/java/com/app/frontend/components/Editor.java
@@ -1,8 +1,28 @@
+
 package com.app.frontend.components;
 
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
+
 public class Editor {
+    private TextArea contentArea;
+
+    public Editor(){
+        this.contentArea= new TextArea();
+        this.contentArea.setWrapText(true);
+    }
+
+    public TextArea getView(){
+        return contentArea;
+    }
+
+    public String getText(){
+        return contentArea.getText();
+    }
+
+    public void setText(String text){
+        contentArea.setText(text);
+    }
 }
