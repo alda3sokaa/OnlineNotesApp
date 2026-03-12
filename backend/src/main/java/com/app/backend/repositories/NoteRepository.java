42fae222af1440ba9028afc613c395bb0ef10d54
oubaiajlouni-code <oubai.ajlouni@ogr.gelisim.edu.tr>
2026-03-13 00:13:36 +0300

    class fixed

--- backend/src/main/java/com/app/backend/repositories/NoteRepository.java
+++ backend/src/main/java/com/app/backend/repositories/NoteRepository.java
@@ -1,5 +1,9 @@
 package com.app.backend.repositories;
 
-public class NoteRepository {
+import com.app.backend.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
 }
