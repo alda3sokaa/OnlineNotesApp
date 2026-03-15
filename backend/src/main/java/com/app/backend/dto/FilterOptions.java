7ba35b0f470d6557622e31e86224e35f7d21ffbc
yusufbassem7 <yusufbassem7@gmail.com>
2026-03-15 11:48:46 +0300

    Update FilterOptions.java

--- backend/src/main/java/com/app/backend/dto/FilterOptions.java
+++ backend/src/main/java/com/app/backend/dto/FilterOptions.java
@@ -1,4 +1,41 @@
 package com.app.backend.dto;
 
+import java.time.LocalDateTime;
+
 public class FilterOptions {
+    private String search;
+    private long userId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 }
