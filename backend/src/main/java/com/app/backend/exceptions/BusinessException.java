4c73c60bc9de7dae3fd87b57141f4f8b9477c37d
yusufbassem7 <yusufbassem7@gmail.com>
2026-03-15 11:49:31 +0300

    Update BusinessException.java

--- backend/src/main/java/com/app/backend/exceptions/BusinessException.java
+++ backend/src/main/java/com/app/backend/exceptions/BusinessException.java
@@ -1,4 +1,7 @@
 package com.app.backend.exceptions;
 
-public class BusinessException {
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
 }
